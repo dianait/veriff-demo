8acb8524070836f776aafd80cf67bc150b68320f
Dianait <dianahdezsoler@gmail.com>
2026-05-09 21:16:28 +0200

    Initial Commit

---
+++ veriffDemo/veriffDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  veriffDemoApp.swift
+//  veriffDemo
+//
+//  Created by Diana Hernandez Soler on 9/5/26.
+//
+
+import SwiftUI
+
+@main
+struct veriffDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
